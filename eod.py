46e6def0d3c7dfc8c4cf049a25f085336e138e34
ninofelino <tutyfelino@gmail.com>
2018-10-03 12:22:56 +0700

    1

--- eod.py
+++ eod.py
@@ -8,13 +8,18 @@ for item in DBF('/home/server/Downloads/INV-HO.DBF',encoding='iso-8859-1'):
     #  print item[1]  
     #  print  list(item.values())
      # print len(item),
-      print item['CODE'],item['DESC1'],item['LQOH']
+     # print item['CODE'],item['DESC1'],item['LQOH']
       statement =" INSERT INTO product_template(id,name,sequence,type,rental,categ_id,list_price,sale_ok,purchase_ok,uom_id,uom_po_id,company_id,active,responsible_id,sale_line_warn,tracking,purchase_line_warn"\
       ") VALUES ("\
-      +item['CODE']+",'"+item['DESC1'].replace("'","")+"',1,'consu',False,1,"+str(item['COSTPRC'])+",True,True,1,1,1,True,1,'no-message','none','no-message') ON CONFLICT ON CONSTRAINT product_template_pkey DO Update set sale_ok=True;"
+      +item['CODE']+",'"+item['DESC1'].replace("'","")+"',1,'consu',False,1,"+str(item['COSTPRC'])+",True,True,1,1,1,True,1,'no-message','none','no-message') ON CONFLICT ON CONSTRAINT product_template_pkey DO Update set sale_ok=True,list_price="+str(item['SELLPRC'])+" ;"
       
      # print item['DESC1']
       cursor.execute(statement)
       conn.commit() 
+      #print statement
+      statement = " INSERT INTO product_product(id,product_tmpl_id,active,barcode,create_uid,write_uid)"\
+      +" values("+item['CODE']+","+item['CODE']+",True,"+item['CODE']+",1,1) ON CONFLICT ON CONSTRAINT product_product_pkey DO UPDATE SET barcode='"+item['CODE']+"',default_code='"+item['CODE']+"';"
       print statement
-    
+      cursor.execute(statement)
+      conn.commit() 
+      
