48e463fed13e51ceeeba0cb0c99a83d0d96c40e6
ninofelino <tutyfelino@gmail.com>
2018-10-03 10:38:29 +0700

    rabu

--- eod.py
+++ eod.py
@@ -8,6 +8,13 @@ for item in DBF('/home/server/Downloads/INV-HO.DBF',encoding='iso-8859-1'):
     #  print item[1]  
     #  print  list(item.values())
      # print len(item),
-      print item['CODE'],item['DESC1']
+      print item['CODE'],item['DESC1'],item['LQOH']
+      statement =" INSERT INTO product_template(id,name,sequence,type,rental,categ_id,list_price,sale_ok,purchase_ok,uom_id,uom_po_id,company_id,active,responsible_id,sale_line_warn,tracking,purchase_line_warn"\
+      ") VALUES ("\
+      +item['CODE']+",'"+item['DESC1'].replace("'","")+"',1,'consu',False,1,"+str(item['COSTPRC'])+",True,True,1,1,1,True,1,'no-message','none','no-message') ON CONFLICT ON CONSTRAINT product_template_pkey DO Update set sale_ok=True;"
+      
      # print item['DESC1']
+      cursor.execute(statement)
+      conn.commit() 
+      print statement
     
