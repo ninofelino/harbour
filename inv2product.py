7072233b8bb7d41302f4b6052d936bebacd43158
ninofelino <tutyfelino@gmail.com>
2018-11-06 16:41:31 +0700

    a

--- inv2product.py
+++ inv2product.py
@@ -1,6 +1,6 @@
 import psycopg2
 import datetime
-conn = psycopg2.connect("host='192.168.1.27' dbname='odoo' user='postgres' password='novellina'")
+conn = psycopg2.connect("host='localhost' dbname='odoo' user='postgres' password='odoo'")
 cursor=conn.cursor()
 cursor.execute("""SELECT * from invvariant""")
 rows = cursor.fetchall()
@@ -18,8 +18,8 @@ for row in rows:
     if jml > 1 :
        #statement=" Insert into Product_product(id,product_tmpl_id,barcode) "\
        #+" values("+row[0]+","+row[0]+",'"+row[0]+"')"\
-       statement=" Insert into Product_attribute_line(id,product_tmpl_id,attribute_id) values("\
-       +row[0]+","+row[0]+",1) ON CONFLICT ON CONSTRAINT product_attribute_line_pkey DO NOTHING;"
+       #statement=" Insert into Product_attribute_line(id,product_tmpl_id,attribute_id) values("\
+       #+row[0]+","+row[0]+",1) ON CONFLICT ON CONSTRAINT product_attribute_line_pkey DO NOTHING;"
        #print statement
        #cursor.execute(statement)
        #conn.commit() 
@@ -30,11 +30,11 @@ cursor.execute("""SELECT * from invproduct_product""")
 rows = cursor.fetchall()
 for row in rows:     
     #print row[0]
-    statement=" Insert into Product_product(id,product_tmpl_id,barcode,default_code,active) "\
-    +" values("+row[0]+","+row[1]+",'"+row[0]+"','"+row[0]+"',True)"\
-    +" ON CONFLICT ON CONSTRAINT product_product_pkey DO UPDATE SET active=True,default_code='"+row[0]+"'"+",barcode='"+row[0]+"'"
-    cursor.execute(statement)
-    conn.commit() 
+    #statement=" Insert into Product_product(id,product_tmpl_id,barcode,default_code,active) "\
+    #+" values("+row[0]+","+row[1]+",'"+row[0]+"','"+row[0]+"',True)"\
+    #+" ON CONFLICT ON CONSTRAINT product_product_pkey DO UPDATE SET active=True,default_code='"+row[0]+"'"+",barcode='"+row[0]+"'"
+    #cursor.execute(statement)
+    #conn.commit() 
     statement=""
     statement="INSERT INTO product_attribute_line_product_attribute_value_rel("\
     +"product_attribute_line_id, product_attribute_value_id) VALUES("\
